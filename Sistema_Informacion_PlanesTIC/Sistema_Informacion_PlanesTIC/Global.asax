91fd99652e6d484730fd2571a17aa70dcd04b68b
nicolasri418 <41582266+nicolasri418@users.noreply.github.com>
2019-06-10 16:36:37 -0500

    Cambio de ruta

---
+++ Sistema_Informacion_PlanesTIC/Sistema_Informacion_PlanesTIC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sistema_Informacion_PlanesTIC.MvcApplication" Language="C#" %>
---
+++ Sistema_Informacion_PlanesTIC/Sistema_Informacion_PlanesTIC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sistema_Informacion_PlanesTIC.Startup))]
+namespace Sistema_Informacion_PlanesTIC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
